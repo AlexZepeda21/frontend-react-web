8d8c63fde18679a3a8ca690ace7b24aff11a70e5
AlexZepeda21 <168386278+AlexZepeda19@users.noreply.github.com>
2024-12-02 10:06:12 -0600

    plantillas de menu desarrolladas.
    plantillas iniciales de menu para administrador y usuario listas para modificación

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 // Importando rutas
 import Login from './login-form';
-import './styles/globals.css'
-
+import './styles/globals.css';
+import Menuadmin from './menus/Menuadmin';
+import Menuuser from './menus/Menuuser';
 
 // Importando rutas de controles
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -16,10 +17,14 @@ function App() {
     <Router>
       <Routes>
         {/* Ruta principal */}
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Menuadmin/>} />
         
         {/* Ruta de login */}
         <Route path="/loginForm" element={<Login />} />
+
+        {/* Ruta de menu admin */}
+        <Route path="/menus/Menuadmin" element={<Menuadmin/>} />
+        <Route path="/menus/Menuuser" element={<Menuuser/>} />
 
 
       </Routes>
