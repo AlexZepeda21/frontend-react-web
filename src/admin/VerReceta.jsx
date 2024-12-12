089b1338613736c283f24b5793350bba176a25b9
AlexZepeda21 <168386278+AlexZepeda19@users.noreply.github.com>
2024-12-12 02:05:33 -0600

    actualización sin NEXT

--- src/admin/VerReceta.jsx
+++ src/admin/VerReceta.jsx
@@ -57,7 +57,7 @@ const Page = () => {
             <Clock className="w-5 h-5 text-gray-600" />
             <div>
               <p className="text-sm text-gray-500">TIEMPO:</p>
-              <p className="font-semibold">{`${tiempo_preparacion} min`}</p>
+              <p className="font-semibold">{tiempo_preparacion} min</p>
             </div>
           </div>
           <div className="flex items-center gap-2">
